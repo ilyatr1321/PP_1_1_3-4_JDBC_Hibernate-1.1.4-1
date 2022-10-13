64026784c91484b078015891c02f2f9fc7081cfa
ilyatr1321 <ilyatr1321@gmail.com>
2022-10-13 10:19:07 +0300

    PP_1_1_3-4_JDBC_Hibernate-1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -13,9 +13,8 @@ import static java.sql.DriverManager.getConnection;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-    private final Connection connection = Util.getConnection();
     public void createUsersTable() {
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
             statement.execute("""
                     CREATE TABLE IF NOT EXISTS `mydbtest`.`users` (
                       `id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -26,30 +25,22 @@ public class UserDaoJDBCImpl implements UserDao {
                       UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)""");
         } catch (SQLException e) {
             e.printStackTrace();
-            try {
-                connection.close();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
             }
         }
 
-    }
+
 
     public void dropUsersTable() {
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
             statement.execute("DROP TABLE IF EXISTS users;");
         } catch (SQLException e) {
-            try {
-                connection.close();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            } throw new RuntimeException(e);
+            throw new RuntimeException(e);
         }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement statement = connection
+        try (Connection connection = Util.getConnection(); PreparedStatement statement = connection
                 .prepareStatement("INSERT users (name, lastName, age)" +
                         "VALUES (?, ?, ?)")) {
             statement.setString(1, name);
@@ -57,30 +48,24 @@ public class UserDaoJDBCImpl implements UserDao {
             statement.setByte(3, age);
             statement.execute();
         } catch (SQLException e) {
-            try {
-                connection.close();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            } throw new RuntimeException(e);
+            throw new RuntimeException(e);
         }
     }
 
     public void removeUserById(long id) {
-        try (PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?;")) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?;")) {
             statement.setLong(1, id);
             statement.execute();
         } catch (SQLException e) {
-            try {
-                connection.close();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            } throw new RuntimeException(e);
+            throw new RuntimeException(e);
         }
     }
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM users")) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM users")) {
             ResultSet res = statement.executeQuery();
             while (res.next()) {
                 User temp = new User(res.getString(2), res.getString(3), res.getByte(4));
@@ -88,11 +73,7 @@ public class UserDaoJDBCImpl implements UserDao {
                 users.add(temp);
             }
         } catch (SQLException e) {
-            try {
-                connection.close();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            } throw new RuntimeException(e);
+            throw new RuntimeException(e);
         }
         return users;
     }
